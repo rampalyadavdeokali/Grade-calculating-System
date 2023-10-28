9746fe6f4f21cceace3bf9f9f05bab63d6756c04
Rampal Yadav <133230206+rampalyadavdeokali@users.noreply.github.com>
2023-10-28 17:09:35 +0530

    Add files via upload
    It is a simple grade system its is used for basic making result for school and collages a simple result .

---
+++ js/grade.js
@@ -0,0 +1,54 @@
+function calculate(){
+    var a=parseInt(document.getElementById('bookOne').value);
+    var b=parseInt(document.getElementById('bookTwo').value);
+    var c=parseInt(document.getElementById('bookThree').value);
+    var d=parseInt(document.getElementById('bookFour').value);
+    var e=parseInt(document.getElementById('bookFive').value);
+
+    if(a>100 || b>100 || c>100 || d>100 ||e>100)
+    {
+        alert("please enter the correct values");
+    }
+    else{
+        var obtain=a+b+c+d+e;
+        document.getElementById("obtain").innerHTML=obtain;
+
+       var per=obtain/500*100;
+       document.getElementById("per").innerHTML=per;
+
+       if(a>33 && b>33 && c>33 && d>33  && e>33)
+       {
+        document.getElementById("remarks").innerHTML="<span style='color:#292'>Pass</span>";
+       }
+       else{
+        document.getElementById("remarks").innerHTML="<span style='color:red'>Fail</span>";
+       }
+
+        if(per>=80)
+        {
+            document.getElementById("grade").textContent="A";
+        }
+        else if(per>=70 && per<80)
+        {
+            document.getElementById("grade").textContent="B";
+        }
+        else if(per>=60 && per<70)
+        {
+            document.getElementById("grade").textContent="C";
+        }
+        else if(per>=50 && per<60)
+        {
+            document.getElementById("grade").textContent="D";
+        }
+        else if(per>=35 && per<50)
+        {
+            document.getElementById("grade").textContent="E";
+        }
+        else
+        {
+            document.getElementById("grade").textContent="F";
+        }
+        
+    }
+    
+}
